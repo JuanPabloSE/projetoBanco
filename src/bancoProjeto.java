0cd8df7f454a61d859fd4f1fb32c7186a76fdec6
JuanPabloSE <juancontas@protonmail.com>
2025-10-31 18:03:12 -0300

    Commit Banco

--- src/bancoProjeto.java
+++ src/bancoProjeto.java
@@ -6,15 +6,17 @@ public class bancoProjeto {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        // SALDO
+        // VARIÁVEIS DO SISTEMA (SALDO, LIMITE DE EMPRÉSTIMO)
         double saldo = 2000.00;
+        double limite, valorTotalJuros, valorParcelas;
 
         // OPÇÃO DESEJADA
         System.out.println("=== BANCO JAVA ===");
-        System.out.println("1 - Ver saldo");
+        System.out.println("1 - Ver Saldo");
         System.out.println("2 - Depositar");
         System.out.println("3 - Sacar");
-        System.out.println("4 - Sair");
+        System.out.println("4 - Solicitar Empréstimo");
+        System.out.println("5 - Sair");
         System.out.println(); // QUEBRA DE LINHA
         System.out.printf("Escolha uma das opções: ");
         int opcao = sc.nextInt();
@@ -57,6 +59,39 @@ public class bancoProjeto {
 
             // SAÍDA DO SISTEMA
             case 4:
+                System.out.print("Qual o valor desejado para empréstimo: ");
+                double emprestimo = sc.nextDouble();
+                System.out.print("Quantidade de parcelas desejada: ");
+                int parcelas = sc.nextInt();
+                System.out.println();                                                  // QUEBRA DE LINHA
+                if (saldo >= 1000.00) {                                                // Se o saldo atual for maior que R$ 1.000, ele pode pegar até 3x o saldo.
+                    limite = saldo * 3;
+                }
+                else {                                                                 // Se o saldo for menor, pode pegar até 2x o saldo.
+                    limite = saldo * 2;
+                }
+                if (emprestimo <= limite) {
+                    int juros = 10;                                                    // 10% DE JUROS.
+                    valorTotalJuros = emprestimo + ((emprestimo * juros) / 100);       // Calcula o valor total com juros.
+                    valorParcelas = valorTotalJuros / parcelas;                        // Divide pelo número de parcelas e mostra o valor de cada uma.
+                    saldo +=emprestimo;                                                // Acrescenta o valor do empréstimo ao saldo.
+                    System.out.println("___________________________________________________________________");
+                    System.out.println("PARABÉNS, SEU EMPRÉSTIMO FOI APROVADO COM SUCESSO EM NOSSO BANCO!");
+                    System.out.println("___________________________________________________________________");
+                    System.out.printf("Valor do empréstimo: R$ %.2f\n", emprestimo);
+                    System.out.printf("Número de parcelas do emprestimo: %d\n", parcelas);
+                    System.out.printf("Juros do empréstimo: %d%%\n", juros);
+                    System.out.printf("Valor total do empréstimo com Juros: R$ %.2f\n", valorTotalJuros);
+                    System.out.printf("Valor das parcelas: R$ %.2f\n",valorParcelas);
+                    System.out.printf("Novo saldo: R$ %.2f\n", saldo);
+                }
+                else {
+                    System.out.println("EMPRÉSTIMO NEGADO - LIMITE PARA EMPRÉSTIMO INSUFICIENTE");
+                }
+
+                break;
+
+            case 5:
                 System.out.println("Saindo do sistema");
                 break;
 
